68d4be43a454ac9a65cc701c41025a69d24ce96f
Donald Batryn <donald.batryn@gmail.com>
2025-11-12 23:26:15 -0600

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,172 @@
+"use client";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+
+const THEMES = [
+  "bronze",
+  "kittens",
+  "laser-tag",
+  "muted",
+  "peach-aqua",
+  "rugrat",
+  "seafoam",
+  "multi",
+  "sunset",
+  "twilight",
+  // "zombie",
+];
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export default function Home() {
+  // function getRandomInt(min, max) {
+  //   min = Math.ceil(min);
+  //   max = Math.floor(max);
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // }
+  const randTheme = THEMES[getRandomInt(0, THEMES.length - 1)];
+  const [theme, setTheme] = useState(randTheme);
+
+  const [tab, setTab] = useState("music");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  console.log("tab: ", tab);
+  console.log("drawer: ", drawerOpen ? "open" : "closed");
+
+  const toggleDrawer = (force = false, newTab = "music") => {
+    if (drawerOpen || force) {
+      if (newTab === "mtvg") {
+        scrollTo(0, 0);
+        setDrawerOpen(false);
+      } else {
+        console.log("hit");
+        scrollTo(0, 850);
+        setDrawerOpen(true);
+      }
+    } else {
+      scrollTo(0, 850);
+      setDrawerOpen(true);
+    }
+  };
+  const handleMenu = (newTab: string) => {
+    console.log("toggleDrawer newTab: ", newTab);
+
+    setTab(newTab);
+    toggleDrawer(false, newTab);
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans mtvg-body">
+      <main className="flex min-h-screen w-full max-w-9xl flex-col items-center justify-between py-8 px-16">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          alt="mtvg logo"
+          src={"/assets/" + theme + ".png"}
+          width={900}
+          height={150}
+          className="w-xxxl"
+          style={{ marginTop: "200px", marginBottom: "200px" }}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <nav id="main-nav" className="flex items-center main-nav">
+          <Image
+            alt="mtvg logo"
+            src={"/assets/" + theme + ".png"}
+            width={140}
+            height={150}
+            className="w-l nav-image"
+            onClick={() => {
+              handleMenu("mtvg");
+            }}
+          />
+          <a onClick={() => handleMenu("music")}>Music</a>
+          <a onClick={() => handleMenu("shows")}>Shows</a>
+          <a onClick={() => handleMenu("merch")}>Merch</a>
+        </nav>
+
+        {tab === "mtvg" && (
+          <div
+            id="mtvg"
+            style={{
+              width: "100%",
+              height: "800px",
+              padding: "16px",
+              marginTop: "32px",
+            }}
+            className="flex flex-col gap-4 text-base font-medium justify-start items-center"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            {THEMES.map((_theme) => {
+              if (_theme !== theme) {
+                return (
+                  <Image
+                    key={_theme}
+                    alt="mtvg logo"
+                    src={"/assets/" + _theme + ".png"}
+                    width={600}
+                    height={150}
+                    style={{}}
+                    className="w-xxxl"
+                    onClick={() => {
+                      setTheme(_theme);
+                      toggleDrawer(true, "mtvg");
+                    }}
+                  />
+                );
+              }
+            })}
+          </div>
+        )}
+        {tab === "music" && (
+          <div
+            id="music"
+            style={{
+              width: "100%",
+              height: "800px",
+              padding: "16px",
+              marginTop: "32px",
+            }}
+            className="flex flex-col gap-4 text-base font-medium justify-start items-center"
           >
-            Documentation
-          </a>
-        </div>
+            Add music here Add music here Add music here Add music here Add
+            music here Add music here Add music here Add music here Add music
+            here Add music here Add music here Add music here Add music here
+          </div>
+        )}
+
+        {tab === "shows" && (
+          <div
+            id="shows"
+            style={{
+              width: "100%",
+              height: "800px",
+              padding: "16px",
+              marginTop: "32px",
+            }}
+            className="flex flex-col gap-4 text-base font-medium justify-start items-center"
+          >
+            Add shows here Add shows here Add shows here Add shows here Add
+            shows here Add shows here Add shows here Add shows here Add shows
+            here Add shows here Add shows here Add shows here Add shows here
+          </div>
+        )}
+
+        {tab === "merch" && (
+          <div
+            id="merch"
+            style={{
+              width: "100%",
+              height: "800px",
+              padding: "16px",
+              marginTop: "32px",
+            }}
+            className="flex flex-col gap-4 text-base font-medium justify-start items-center"
+          >
+            Add merch here Add merch here Add merch here Add merch here Add
+            merch here Add merch here Add merch here Add merch here Add merch
+            here Add merch here Add merch here Add merch here Add merch here
+          </div>
+        )}
       </main>
     </div>
   );
